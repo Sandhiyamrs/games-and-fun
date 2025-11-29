853f22df5987bd1d5fd9f542f611f5affc0131c9
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-11-29 22:23:21 +0530

    Update file1_general_knowledge_quiz.py

--- 03_quiz_games/file1_general_knowledge_quiz.py
+++ 03_quiz_games/file1_general_knowledge_quiz.py
@@ -1,1 +1,17 @@
+# General Knowledge Quiz
+questions = {
+    "Capital of France?": "Paris",
+    "Largest ocean?": "Pacific",
+    "Fastest land animal?": "Cheetah"
+}
+
+score = 0
+for q,a in questions.items():
+    ans = input(q + " ")
+    if ans.strip().lower() == a.lower():
+        print("Correct!")
+        score += 1
+    else:
+        print(f"Wrong! Answer: {a}")
+print(f"Your score: {score}/{len(questions)}")
 
