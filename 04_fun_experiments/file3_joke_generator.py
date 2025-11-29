3a49b35bd3b6b56ec939ce3bd884a4440c3ad32b
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-11-29 22:25:13 +0530

    Update file3_joke_generator.py

--- 04_fun_experiments/file3_joke_generator.py
+++ 04_fun_experiments/file3_joke_generator.py
@@ -1,1 +1,10 @@
+# Joke Generator
+jokes = [
+    "Why do programmers prefer dark mode? Because light attracts bugs!",
+    "Why do Java developers wear glasses? Because they don’t C#!",
+    "Debugging is like being the detective in a crime movie where you are also the murderer."
+]
+
+import random
+print(random.choice(jokes))
 
