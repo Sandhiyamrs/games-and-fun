294473b65b3277f70580751ae77fab0f5eda8f7f
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-11-29 22:23:39 +0530

    Update file2_math_quiz.py

--- 03_quiz_games/file2_math_quiz.py
+++ 03_quiz_games/file2_math_quiz.py
@@ -1,1 +1,15 @@
+# Math Quiz
+import random
+
+score = 0
+for i in range(5):
+    a = random.randint(1,20)
+    b = random.randint(1,20)
+    ans = int(input(f"{a} + {b} = "))
+    if ans == a+b:
+        print("Correct!")
+        score += 1
+    else:
+        print(f"Wrong! Answer: {a+b}")
+print(f"Your score: {score}/5")
 
