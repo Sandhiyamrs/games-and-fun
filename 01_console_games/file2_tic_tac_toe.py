aa9ad16d7e7f23d0033e1d0fe4e70183687f03c2
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-11-29 22:20:30 +0530

    Update file2_tic_tac_toe.py

--- 01_console_games/file2_tic_tac_toe.py
+++ 01_console_games/file2_tic_tac_toe.py
@@ -1,1 +1,39 @@
+# Tic-Tac-Toe Game (Console)
+
+board = [" "]*9
+
+def show():
+    print(board[0],"|",board[1],"|",board[2])
+    print("--+---+--")
+    print(board[3],"|",board[4],"|",board[5])
+    print("--+---+--")
+    print(board[6],"|",board[7],"|",board[8])
+
+def check_winner(mark):
+    wins = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ]
+    for c in wins:
+        if board[c[0]]==board[c[1]]==board[c[2]]==mark:
+            return True
+    return False
+
+turn = "X"
+for i in range(9):
+    show()
+    pos = int(input(f"Player {turn}, choose (0-8): "))
+    if board[pos] == " ":
+        board[pos] = turn
+    else:
+        print("Already occupied! Try again.")
+        continue
+
+    if check_winner(turn):
+        show()
+        print(f"Player {turn} wins!")
+        break
+
+    turn = "O" if turn=="X" else "X"
 
