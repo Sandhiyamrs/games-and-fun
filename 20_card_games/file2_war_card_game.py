6dfd5e03a1e4bed54715f2e9b297decea31572e9
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-10 20:47:53 +0530

    Update file2_war_card_game.py

--- 20_card_games/file2_war_card_game.py
+++ 20_card_games/file2_war_card_game.py
@@ -1,1 +1,10 @@
+# Extremely simple war game for one round
+import random
+card_values = list(range(2,15))*4
+random.shuffle(card_values)
+p1=card_values.pop(); p2=card_values.pop()
+print(f"Player1: {p1} vs Player2: {p2}")
+if p1>p2: print("Player1 wins")
+elif p2>p1: print("Player2 wins")
+else: print("Tie - War!")
 
