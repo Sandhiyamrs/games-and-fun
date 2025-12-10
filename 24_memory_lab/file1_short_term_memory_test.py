8700ffc8a02054ae05954bd720254d07d30f3ca4
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-10 20:54:02 +0530

    Update file1_short_term_memory_test.py

--- 24_memory_lab/file1_short_term_memory_test.py
+++ 24_memory_lab/file1_short_term_memory_test.py
@@ -1,1 +1,9 @@
+# Show a short sequence to memorize
+import random, time
+seq = [random.randint(0,9) for _ in range(5)]
+print("Memorize this:", " ".join(map(str,seq)))
+time.sleep(3)
+print("\n" * 40)
+ans = input("Enter the sequence: ").strip().split()
+print("Correct!" if list(map(str,seq))==ans else "Wrong. Was: "+" ".join(map(str,seq)))
 
