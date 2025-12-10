468b243550803c062a93de817f132b7f8f2d39a1
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-10 20:49:10 +0530

    Update file1_debug_the_code.py

--- 21_coding_challenge_games/file1_debug_the_code.py
+++ 21_coding_challenge_games/file1_debug_the_code.py
@@ -1,1 +1,6 @@
+# Present a broken snippet and ask user to identify bug
+print("Find the bug in the code below:")
+print("for i in range(5):\n    print(i)\n")
+ans = input("What is wrong? (type: 'nothing' if fine): ")
+print("Thanks! You answered:", ans)
 
