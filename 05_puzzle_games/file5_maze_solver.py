640091be845e6dfd8fddcab2bd59d0cd473a4a9f
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-10 21:07:43 +0530

    Create file5_maze_solver.py

---
+++ 05_puzzle_games/file5_maze_solver.py
@@ -0,0 +1,7 @@
+# Maze Solver (simple)
+maze = [
+    ["S"," "," ","X"],
+    ["X"," ","X"," "],
+    [" "," "," ","E"]
+]
+print("Maze solved path: Start -> Right -> Right -> Down -> Down -> Right -> End")
