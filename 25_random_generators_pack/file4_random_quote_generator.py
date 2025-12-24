39e52fb784b93b5ae5e212dc1ade61c222835671
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-24 22:41:23 +0530

    Create file4_random_quote_generator.py

---
+++ 25_random_generators_pack/file4_random_quote_generator.py
@@ -0,0 +1,10 @@
+import random
+
+quotes = [
+    "Code is like humor. When you have to explain it, it’s bad.",
+    "First, solve the problem. Then, write the code.",
+    "Experience is the name everyone gives to their mistakes."
+]
+
+print("💬 Random Quote:")
+print(random.choice(quotes))
