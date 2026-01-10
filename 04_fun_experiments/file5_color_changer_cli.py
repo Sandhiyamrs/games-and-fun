5813a978d15b87f92e3c9e54b90eef2a2b685f5f
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-10 22:47:13 +0530

    Update file5_color_changer_cli.py

--- 04_fun_experiments/file5_color_changer_cli.py
+++ 04_fun_experiments/file5_color_changer_cli.py
@@ -1,17 +1,4 @@
-import time
-import itertools
+import random
 
-colors = [
-    "\033[91m",  # Red
-    "\033[92m",  # Green
-    "\033[93m",  # Yellow
-    "\033[94m",  # Blue
-    "\033[95m",  # Magenta
-    "\033[96m",  # Cyan
-]
-
-print("🎨 Color Changing Text")
-
-for color in itertools.cycle(colors):
-    print(color + "Python is Fun!" + "\033[0m", end="\r")
-    time.sleep(0.4)
+colors = ["Red", "Green", "Blue", "Yellow", "Purple"]
+print("🎨 Random Color:", random.choice(colors))
