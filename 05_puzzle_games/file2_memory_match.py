15b53254da92c88fa097805b0cf6ce1973b409e0
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-10 22:48:50 +0530

    Update file2_memory_match.py

--- 05_puzzle_games/file2_memory_match.py
+++ 05_puzzle_games/file2_memory_match.py
@@ -1,29 +1,13 @@
 import random
-import time
 
-cards = ["A", "A", "B", "B", "C", "C"]
-random.shuffle(cards)
+numbers = [1,2,3,4,5]
+random.shuffle(numbers)
+print("Memorize:", numbers)
+input("Press Enter to hide...")
+print("\n" * 20)
 
-print("🧠 Memory Match Game!")
-print("There are 6 cards (positions 1 to 6). Try to find pairs.\n")
-
-revealed = ["*", "*", "*", "*", "*", "*"]
-attempts = 0
-
-while "*" in revealed:
-    print("\nCards:", " ".join(revealed))
-    
-    first = int(input("Pick first card (1-6): ")) - 1
-    second = int(input("Pick second card (1-6): ")) - 1
-
-    attempts += 1
-
-    if cards[first] == cards[second]:
-        print("✔ Matched:", cards[first])
-        revealed[first] = cards[first]
-        revealed[second] = cards[second]
-    else:
-        print("❌ No Match")
-        time.sleep(1)
-
-print("\n🎉 You matched all cards in", attempts, "attempts!")
+guess = input("Enter numbers in order: ").split()
+if guess == list(map(str, numbers)):
+    print("🎉 Correct!")
+else:
+    print("❌ Wrong!")
