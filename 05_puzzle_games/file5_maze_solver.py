31fe7a2f906d8ef70bb7dc39e51306d2feff509d
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-10 22:49:55 +0530

    Update file5_maze_solver.py

--- 05_puzzle_games/file5_maze_solver.py
+++ 05_puzzle_games/file5_maze_solver.py
@@ -1,7 +1,7 @@
-# Maze Solver (simple)
-maze = [
-    ["S"," "," ","X"],
-    ["X"," ","X"," "],
-    [" "," "," ","E"]
-]
-print("Maze solved path: Start -> Right -> Right -> Down -> Down -> Right -> End")
+word = "python"
+guess = input("Enter crossword word: ")
+
+if guess.lower() == word:
+    print("✅ Correct!")
+else:
+    print("❌ Incorrect!")
