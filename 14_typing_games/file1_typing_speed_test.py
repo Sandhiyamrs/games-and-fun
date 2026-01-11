19559eb443db69c81040363326248845dc79611f
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-11 22:37:42 +0530

    Update file1_typing_speed_test.py

--- 14_typing_games/file1_typing_speed_test.py
+++ 14_typing_games/file1_typing_speed_test.py
@@ -1,10 +1,7 @@
-import time
+print("🔺 Shape Learning")
 
-text = "Practice makes you perfect in typing!"
-
-print(text)
-start = time.time()
-input("Type the above line: ")
-end = time.time()
-
-print(f"\nTime taken: {end - start:.2f} seconds")
+shape = input("Which shape has 3 sides? ").lower()
+if shape == "triangle":
+    print("Correct!")
+else:
+    print("Incorrect!")
