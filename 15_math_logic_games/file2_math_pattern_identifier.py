6b84c9fa1ee48f86130ab63b7ee3660115268d9a
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-11 22:39:43 +0530

    Update file2_math_pattern_identifier.py

--- 15_math_logic_games/file2_math_pattern_identifier.py
+++ 15_math_logic_games/file2_math_pattern_identifier.py
@@ -1,9 +1,3 @@
-patterns = {
-    "2, 4, 6, 8, ?" : "10",
-    "3, 9, 27, ?" : "81",
-    "1, 1, 2, 3, 5, ?" : "8"
-}
-
-for seq, ans in patterns.items():
-    guess = input(f"Find the next number → {seq}: ")
-    print("Correct!" if guess == ans else f"Wrong! Correct answer: {ans}")
+pattern = [2,4,6,8]
+guess = int(input("Next number after 8? "))
+print("Correct!" if guess == 10 else "Wrong!")
