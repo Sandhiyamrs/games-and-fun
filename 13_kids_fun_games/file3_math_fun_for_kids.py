5472996bb46497f7fbc6646401ae55b8a5006f58
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-11 22:36:51 +0530

    Update file3_math_fun_for_kids.py

--- 13_kids_fun_games/file3_math_fun_for_kids.py
+++ 13_kids_fun_games/file3_math_fun_for_kids.py
@@ -1,3 +1,5 @@
+print("➕ Math Fun")
+
 a, b = 2, 3
-ans = int(input(f"What is {a}+{b}? "))
-print("Correct!" if ans == a+b else "Try again!")
+ans = int(input(f"What is {a} + {b}? "))
+print("Correct!" if ans == a+b else "Wrong!")
