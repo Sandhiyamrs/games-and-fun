50eae5acf710876135ad102880f237ffcd4a42b9
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-12 22:52:03 +0530

    Update file2_rhythm_tap_game.py

--- 17_music_and_sound_games/file2_rhythm_tap_game.py
+++ 17_music_and_sound_games/file2_rhythm_tap_game.py
@@ -1,18 +1,9 @@
-# Simulated rhythm tap (measures reaction consistency)
 import time
 
-print("Rhythm Tap Game")
-input("Get ready... Press Enter to start")
-print("Tap Enter 3 times to the beat!")
-times = []
-for i in range(3):
-    input()
-    times.append(time.time())
-intervals = [round(times[i]-times[i-1],2) for i in range(1,len(times))]
-if len(intervals) < 2:
-    score = "Too few taps"
-else:
-    variance = max(intervals) - min(intervals)
-    score = "Good" if variance < 0.3 else "OK" if variance < 0.8 else "Poor"
-print("Done! Your timing score:", score)
+print("Tap ENTER when ready!")
+time.sleep(2)
+start = time.time()
+input()
+end = time.time()
 
+print("Reaction time:", round(end - start, 2), "seconds")
