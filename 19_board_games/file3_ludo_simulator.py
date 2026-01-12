a95c5f2c00c51f3c2698e783cabd96cb1c7b2b33
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-12 22:55:30 +0530

    Update file3_ludo_simulator.py

--- 19_board_games/file3_ludo_simulator.py
+++ 19_board_games/file3_ludo_simulator.py
@@ -1,10 +1,3 @@
-# Very simple Ludo dice mover for one token
-position = 0
-print("Ludo Simulator")
-for i in range(5):
-    input("Press Enter to roll dice")
-    import random
-    roll=random.randint(1,6)
-    position+=roll
-    print(f"Rolled {roll}. New position: {position}")
-
+import random
+dice = random.randint(1,6)
+print("Dice rolled:", dice)
