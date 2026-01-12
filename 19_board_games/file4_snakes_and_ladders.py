c26da35c4abe65f127fee5c4e6cfc813cb16bb02
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-12 22:55:56 +0530

    Update file4_snakes_and_ladders.py

--- 19_board_games/file4_snakes_and_ladders.py
+++ 19_board_games/file4_snakes_and_ladders.py
@@ -1,6 +1,8 @@
-position = 0
-while position < 20:
-    roll = int(input("Roll dice: "))
-    position += roll
-    print("Position:", position)
-print("You won!")
+print("Othello starting board")
+board = [["." for _ in range(8)] for _ in range(8)]
+board[3][3] = "W"
+board[4][4] = "W"
+board[3][4] = "B"
+board[4][3] = "B"
+for r in board:
+    print(" ".join(r))
